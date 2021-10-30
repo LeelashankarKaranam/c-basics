45b773efc1b546fcbdf25087b7c8ab658e9a7c9f
LeelashankarKaranam <92910516+LeelashankarKaranam@users.noreply.github.com>
2021-10-30 00:01:20 -0700

    Add files via upload

---
+++ 30-10-2021  class.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+	//variable
+	int shankar,rajesh,sum;
+	//initialization
+	shankar=123;
+	rajesh=456;
+	//calculations
+	sum=shankar+rajesh;
+	//output
+	printf("%d",sum
+);
+}
---
+++ 30-10-2021-3.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main()
+{
+	int shankar =123,rajesh =456, sum =shankar+rajesh;
+	printf("sum of %d and %d is %d",shankar,rajesh,sum);
+}
